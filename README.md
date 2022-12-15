97ee5bfca69dd271236fc7f083137bf5ff5e263a
Jamie Nelson <jamie@wearsafe.com>
2022-12-15 13:51:04 -0500

    ci(workflow): remove .trunk links

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ This action will find the issue keys in the GitHub event and return them as a co
 <!-- start usage -->
 
 ```yaml
-- uses: bitflight-devops/github-action-jira-find-issue-keys@v1.1.0
+- uses: bitflight-devops/github-action-jira-find-issue-keys@v1.1.3
   with:
     # The github token used for authenticating to GitHub
     token: ''
--- release.sh
+++ release.sh
@@ -26,9 +26,9 @@ git tag -d "${stub_major}" 2>/dev/null || true
 git tag -d "${stub_major_minor}" 2>/dev/null || true
 git tag -a "${stub_major}" -m "Release ${newtag}"
 git tag -a "${stub_major_minor}" -m "Release ${newtag}"
-git push origin ":${stub_major}" 2>/dev/null || true
-git push origin ":${stub_major_minor}" 2>/dev/null || true
+# git push origin ":${stub_major}" 2>/dev/null || true
+# git push origin ":${stub_major_minor}" 2>/dev/null || true
 yarn postversion
 git push
-git push --tags
+git push -f --tags
 # yarn release:post
